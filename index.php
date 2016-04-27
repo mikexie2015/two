b4e6cc3b8edcc2e9a4100baa19637e70ebb492e7
mikexie <1413519894@qq.com>
2016-04-27 13:24:10 +0800

    更新

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 // +----------------------------------------------------------------------
 // | OneThink [ WE CAN DO IT JUST THINK IT ]
 // +----------------------------------------------------------------------
@@ -7,30 +8,33 @@
 // | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
 // +----------------------------------------------------------------------
 
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+if (version_compare(PHP_VERSION, '5.3.0', '<'))
+    die('require PHP > 5.3.0 !');
 
 /**
  * 系统调试设置
  * 项目正式部署后请设置为false
  */
-define('APP_DEBUG', true );
+define('APP_DEBUG', true);
 
 /**
  * 应用目录设置
  * 安全期间，建议安装调试完成后移动到非WEB目录
  */
-define ( 'APP_PATH', './Application/' );
+define('APP_PATH', './');
+define('TMPL_PATH', './Tpl/');
+define('PIC_PATH', './Pic/');
 
-if(!is_file(APP_PATH . 'User/Conf/config.php')){
-	header('Location: ./install.php');
-	exit;
+if (!is_file(APP_PATH . 'User/Conf/config.php')) {
+    header('Location: ./install.php');
+    exit;
 }
 
 /**
  * 缓存目录设置
  * 此目录必须可写，建议移动到非WEB目录
  */
-define ( 'RUNTIME_PATH', './Runtime/' );
+define('RUNTIME_PATH', './Cache/');
 
 /**
  * 引入核心入口
